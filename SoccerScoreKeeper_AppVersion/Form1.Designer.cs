ba5cedd02bf50133cfaf5a8ec221a9e9cb90c992
Peter Anguelov <pet.anguelov@gmail.com>
2025-11-13 22:05:40 +0200

    Add project files.

---
+++ SoccerScoreKeeper_AppVersion/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SoccerScoreKeeper_AppVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnRemovePoint = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(84, 99);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Add a point";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnAddPoint_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(237, 257);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(35, 13);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
+            // 
+            // btnRemovePoint
+            // 
+            this.btnRemovePoint.Location = new System.Drawing.Point(75, 128);
+            this.btnRemovePoint.Name = "btnRemovePoint";
+            this.btnRemovePoint.Size = new System.Drawing.Size(95, 23);
+            this.btnRemovePoint.TabIndex = 2;
+            this.btnRemovePoint.Text = "Remove a point";
+            this.btnRemovePoint.UseVisualStyleBackColor = true;
+            this.btnRemovePoint.Click += new System.EventHandler(this.btnRemovePoint_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(84, 157);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset Points";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRemovePoint);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnRemovePoint;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ SoccerScoreKeeper_AppVersion/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoccerScoreKeeper_AppVersion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int num = 0;
+        private void btnAddPoint_Click(object sender, EventArgs e)
+        {
+            num++;
+            lblHelloWorld.Text = num.ToString();
+        }
+
+        private void btnRemovePoint_Click(object sender, EventArgs e)
+        {
+            if (num <= 0)
+            {
+                num = 0;
+            } else
+            {
+                num--;
+                lblHelloWorld.Text = num.ToString();
+            }
+            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            num = 0;
+            lblHelloWorld.Text = num.ToString();
+        }
+    }
+}
